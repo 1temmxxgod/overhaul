967ebd1bca2241292da8db871e6b5fc12babc591
1temmxxgod <152515503+1temmxxgod@users.noreply.github.com>
2025-03-11 21:22:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+//TG
+document.getElementById('participate-telegram1').addEventListener('click', function () {
+    // Здесь можно добавить логику для участия, например, перенаправление в Telegram
+    alert('Нажмите "ОК", чтобы перейти в Telegram');
+    window.location.href = 'https://t.me/ov3rhau1'; // Замените на ссылку на ваш Telegram-бот
+});
+//TG
+document.getElementById('participate-telegram2').addEventListener('click', function () {
+    // Здесь можно добавить логику для участия, например, перенаправление в Telegram
+    alert('Нажмите "ОК", чтобы перейти в Telegram');
+    window.location.href = 'https://t.me/Test_Overhaul_Bot'; // Замените на ссылку на ваш Telegram-бот
+});
+//TG
+document.getElementById('participate-telegram3').addEventListener('click', function () {
+    // Здесь можно добавить логику для участия, например, перенаправление в Telegram
+    alert('Нажмите "ОК", чтобы перейти в Telegram');
+    window.location.href = 'https://t.me/manager_overhaul'; // Замените на ссылку на ваш Telegram-бот
+});
+//YT
+document.getElementById('participate-youtube1').addEventListener('click', function () {
+    // Здесь можно добавить логику для участия, например, перенаправление в Telegram
+    alert('Нажмите "ОК", чтобы перейти в YouTube');
+    window.location.href = 'https://youtube.com/@cfudik1337'; // Замените на ссылку на ваш Telegram-бот
+});
+//YT
+document.getElementById('participate-youtube2').addEventListener('click', function () {
+    // Здесь можно добавить логику для участия, например, перенаправление в Telegram
+    alert('Нажмите "ОК", чтобы перейти в YouTube');
+    window.location.href = 'https://youtube.com/@Kiruha-vibil'; // Замените на ссылку на ваш Telegram-бот
+});
+//DS
+document.getElementById('participate-discord').addEventListener('click', function () {
+    // Здесь можно добавить логику для участия, например, перенаправление в Telegram
+    alert('Нажмите "ОК", чтобы перейти в Discord');
+    window.location.href = 'https://discord.gg/mtxr5dFtZk'; // Замените на ссылку на ваш Telegram-бот
+});
+//EMAIL
+document.getElementById('participate-email').addEventListener('click', function () {
+    // Здесь можно добавить логику для участия, например, перенаправление в Telegram
+    alert('Нажмите "ОК", чтобы перейти в Email');
+    window.location.href = 'https://mail.google.com/mail/u/1/#inbox?compose=new'; // Замените на ссылку на ваш Telegram-бот
+});
